35f1b70777aa76b2b18855ce7548ead90af318b4
Nofipriyanto <refliyantokasim123@gmail.com>
2024-09-09 08:07:02 +0800

    calculator

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
 
-// apa saja
 class CalculatorApp extends StatelessWidget {
   const CalculatorApp({Key? key}) : super(key: key);
 
